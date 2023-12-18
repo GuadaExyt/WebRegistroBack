61d67ce1e3d579e96f9673b23235c6aa0fd6ee03
Guada <guada@Air-de-Guada.home>
2023-12-18 17:09:26 +0100

    Conectando front y back y añadiendo update a la propiedad done

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from pydantic import BaseModel
 from datetime import datetime
 from google.cloud import datastore
 from http import server
+from fastapi.middleware.cors import CORSMiddleware
 
 client = datastore.Client()
 
 #FORMULARIO
 
 #FUNCION PARA PROCESAR EL FORMULARIO
-def process_form(name, ts):
+def process_form(name, ts, done=True):
     #name = request.form["name"]
     timestamp = datastore.Entity(client.key("Timestamp", name))
     timestamp.update({
         "name": name,
         "time": ts,
+        "done": done,
     })
 
     client.put(timestamp)
@@ -40,5 +42,14 @@ class MessageBody(BaseModel):
 def create_timestamp(message_body: MessageBody):
     current_time = datetime.now()
     print(f" Nombre: {message_body.name}, Timestamp: {current_time}")
-    process_form(message_body.name, current_time)
+    process_form(message_body.name, current_time, done=True)
     return {"name": f"{message_body.name}"}
+
+#PERMITIR SOLICITUDES DESDE EL DOMINIO DE MI APLICACIÓN
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
