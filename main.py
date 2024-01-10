c8fc33c302d398835534490c54dbf1a6f237d4f6
Exyt <exyt@Mini-de-Exyt.home>
2024-01-10 11:08:57 +0100

    ✨ feat: añadir método GET /photo

--- main.py
+++ main.py
@@ -14,16 +14,6 @@ app = FastAPI()
 cred = credentials.Certificate("credentials/credentialsfirebase.json")
 firebase_admin.initialize_app(cred)
 
-# uid = 'some-uid'
-# custom_token = auth.create_custom_token(uid)
-# print(f"this is a custom token: {custom_token}")
-
-# TIENE QUE IR DENTRO DE LA FUNCION 
-# decoded_token = auth.verify_id_token(idToken)
-# uid = decoded_token['uid']
-
-# print(f"This is the decoded token {decoded_token}")
-
 class MessageBody(BaseModel):
     name: str
     file_url: str 
@@ -31,15 +21,16 @@ class MessageBody(BaseModel):
 #FORMULARIO
 
 #FUNCION PARA PROCESAR EL FORMULARIO
-def process_form(name, ts, file_url, done=True):
+def process_form(name, ts, file_url, user_id,  done=True):
     #name = request.form["name"]
     timestamp = datastore.Entity(client.key("Photo"))
     timestamp.update({
         "name": name,
         "time": ts,
         "done": done,
-        "file_url": file_url
-    })
+        "file_url": file_url,
+        "user_id": user_id,
+        })
 
     client.put(timestamp)
     return "Has sido registrado"
@@ -51,7 +42,9 @@ def create_timestamp(message_body: MessageBody, authorization: str = Header(...)
     try: 
         token = authorization.replace("Bearer ", "")
         decoded_token = auth.verify_id_token(token)
-        print(decoded_token)
+        user_id = decoded_token.get('uid')
+        print("DECODED TOKEN:", decoded_token)
+
 
     except Exception as e:
         print(e)
@@ -59,17 +52,50 @@ def create_timestamp(message_body: MessageBody, authorization: str = Header(...)
     
     try:
         current_time = datetime.now()
+
         #GENERA NOMBRE ÚNICO USANDO UUID 
-        file_name = str(uuid.uuid4()) + '_' + message_body.name
-        print(f"Nombre: {message_body.name}, Timestamp: {current_time}, File URL: {message_body.file_url}")
-        process_form(file_name , current_time, message_body.file_url, done=True)
+        file_name = message_body.name
+        print(f"Nombre: {message_body.name}, Timestamp: {current_time}, File URL: {message_body.file_url}, User_id: {user_id}")
+        process_form(file_name , current_time, message_body.file_url, user_id, done=True)
         result = {"message": "Registro exitoso"}  
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail=f"Error en el proceso: {str(e)}")  # DEVUELVE ERROR
     
     return result
+    
 
+#✨ feat: añadir método GET /photo
+@app.get("/photo/")
+def get_user_photos(authorization: str = Header(...)):
+    try: 
+        token = authorization.replace("Bearer ", "")
+        decoded_token = auth.verify_id_token(token)
+        user_id = decoded_token.get('uid')
+        print("ID de usuario:", user_id)
+
+        print(decoded_token)
+
+    except Exception as e:
+        print(e)
+        raise  HTTPException(status_code=401, detail=f"Error en la autenticación")
+    
+    try: 
+        query = client.query(kind="Photo")
+        query.add_filter("user_id", "=", user_id)
+        # print("query IMPRESO:", query) 
+        
+        photos = list(query.fetch())
+        user_photo_urls = [photo.get("file_url")for photo in photos] 
+        
+
+        print("URLs de las fotos del usuario:", user_photo_urls)
+        return {"photo_urls": user_photo_urls}
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=400, detail=f"Error en la consulta: {str(e)}")
+    
 #PERMITIR SOLICITUDES DESDE EL DOMINIO DE MI APLICACIÓN
 app.add_middleware(
     CORSMiddleware,
